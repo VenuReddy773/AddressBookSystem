1ba9d72cd3cea5cd2e5f5447a0d40293ce097605
“VenuReddy773” <venu706@gmail.com>
2021-11-16 16:24:55 +0530

    [VenuGopalReddy] Added: Created Address Book Program

---
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace AddressBookSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Welcome to Address Book Program\n");
+                Console.WriteLine("Choose an Option Execute Program\n 1.CreateContact\n 2.Exit\n ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter The Details as Firstname,Lastname,Address,City,State,Zip,PhoneNumber,Email\n");
+                        Contact contact = new Contact()
+                        {
+                            FirstName = Console.ReadLine(),
+                            LastName = Console.ReadLine(),
+                            Address = Console.ReadLine(),
+                            City = Console.ReadLine(),
+                            State = Console.ReadLine(),
+                            Zip = Console.ReadLine(),
+                            PhoneNumber = Console.ReadLine(),
+                            Email = Console.ReadLine(),
+                        };
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Enter Correct Option\n");
+                        break;
+
+                }
+            }
+        }
+    }
+}
+
