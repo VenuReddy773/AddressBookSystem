867a20791463f72a713fcfa46751912b2a250855
“VenuReddy773” <venu706@gmail.com>
2021-11-17 19:23:17 +0530

    [VenuGopalReddy] Added: Edit Contact Feature

--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -6,13 +6,14 @@ namespace AddressBookSystem
     {
         static void Main(string[] args)
         {
+            Contact contact = new Contact();
+            AddressBook addressBook = new AddressBook();
             bool flag = true;
             while (flag)
             {                
                 Console.WriteLine("\nWelcome to Address Book Program!!!");
-                Console.WriteLine("Choose an Option Execute Program\n 1.CreateContact\n 2.AddContact\n 3.Exit\n ");
-                int option = Convert.ToInt32(Console.ReadLine());
-                Contact contact = new Contact();
+                Console.WriteLine("Choose an Option Execute Program\n 1.CreateContact\n 2.AddContact\n 3.EditContact\n 4.Exit\n ");
+                int option = Convert.ToInt32(Console.ReadLine());                
                 switch (option)
                 {
                     case 1:
@@ -31,12 +32,17 @@ namespace AddressBookSystem
                         break;
                     case 2:
                         Console.WriteLine("Enter The Details in Order of Firstname,Lastname,Address,City,State,Zip,PhoneNumber,Email... To add contact..\n");
-                        AddressBook addressBook = new AddressBook();
-                        Contact addcontact = new Contact();
                         addressBook.Addcontact(contact);
                         addressBook.Display();
                         break;
-                    case 3:
+                    case 3:                    
+                        Console.WriteLine("Enter Contact Name to Edit Details..\n");
+                        string name = Console.ReadLine();
+                        addressBook.EditContact(name);
+                        Console.WriteLine("Edited Successfully\n");
+                        addressBook.Display();
+                        break;
+                    case 4:
                         flag = false;
                         break;
                     default:
